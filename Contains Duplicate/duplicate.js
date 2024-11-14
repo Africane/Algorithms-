84a5d5e0d29b857776c3cbbff949a8eeed738a52
David Owino <daveowino753@gmail.com>
2024-11-14 05:48:18 +0300

    updated duplicate.js

--- Contains Duplicate/duplicate.js
+++ Contains Duplicate/duplicate.js
@@ -1,3 +1,13 @@
 // Given an integer array nums, return true if any value appears at least twice in 
 // the array, and return false if every element is distinct.
 
+var containsDuplicate = function(nums) {
+    let seen = new Set();
+    for (let num of nums) {
+        if (seen.has(num)) {
+            return true;
+        }
+        seen.add(num);
+    }
+    return false;
+}
