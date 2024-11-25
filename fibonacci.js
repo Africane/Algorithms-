acfe7cf69411c18fec53b5011cf1fb416ce891c7
David Owino <daveowino753@gmail.com>
2024-11-25 10:08:19 +0300

    completed fib file

--- fibonacci.js
+++ fibonacci.js
@@ -2,3 +2,9 @@
 // n-th number of the Fibonacci sequence. The first and second number of the sequence is 1. To
 // generate the next number of the sequence, we sum the previous two.
 
+const fib = (n) => {
+    if (n<=2) return 1;
+    return fib(n-1) + fib(n-2);
+}
+
+console.log(fib(6)); // 8
