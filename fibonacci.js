d03249af420daef43364d61265914aafb3b04d67
David Owino <daveowino753@gmail.com>
2024-11-25 10:05:44 +0300

    added file fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,4 @@
+// write a function fib(n) that takes in a number as an argument. The function should return the
+// n-th number of the Fibonacci sequence. The first and second number of the sequence is 1. To
+// generate the next number of the sequence, we sum the previous two.
+
