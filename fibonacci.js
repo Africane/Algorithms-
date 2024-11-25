4461b3813249ae80abdddc1a75de1d12bd9ff73a
David Owino <daveowino753@gmail.com>
2024-11-25 10:57:52 +0300

    added file fib

--- fibonacci.js
+++ fibonacci.js
@@ -8,3 +8,10 @@ const fib = (n) => {
 }
 
 console.log(fib(6)); // 8
+console.log(fib(7)); // 13
+console.log(fib(50)); // 21
+
+// write a function fib(n) that takes in a number as an argument. The function should return the
+// n-th number of the Fibonacci sequence. The first and second number of the sequence is 1. To
+// generate the next number of the sequence, we sum the previous two.
+
